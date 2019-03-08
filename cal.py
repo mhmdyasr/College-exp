109cb86cff25e5857a0854ff71903f4bf0be58b5
A MUHAMMED YASIR <36158258+mhmdyasr@users.noreply.github.com>
2019-03-08 09:55:09 +0530

    Updated
    New line

--- cal.py
+++ cal.py
@@ -2,11 +2,10 @@ import MyCalc.add.addition
 import MyCalc.sub.subtraction
 import MyCalc.div.division
 import MyCalc.mul.multiplication
-
 a=int(input("enter the num:"))
 b=int(input("enter the num:"))
-
 print("addition opertaion is:",MyCalc.add.addition.add(a,b))
 print("subtraction operation is:",MyCalc.sub.subtraction.sub(a,b))
 print("division operation is:",MyCalc.div.division.div(a,b))
 print("multiplication operation is:",MyCalc.mul.multiplication.mul(a,b))
+//Added new line

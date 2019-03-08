83533be7ac2717f6435516cf4145b7980204eadc
A MUHAMMED YASIR <36158258+mhmdyasr@users.noreply.github.com>
2019-03-08 09:53:51 +0530

    Added new python file
    OSS/User

---
+++ cal.py
@@ -0,0 +1,12 @@
+import MyCalc.add.addition
+import MyCalc.sub.subtraction
+import MyCalc.div.division
+import MyCalc.mul.multiplication
+
+a=int(input("enter the num:"))
+b=int(input("enter the num:"))
+
+print("addition opertaion is:",MyCalc.add.addition.add(a,b))
+print("subtraction operation is:",MyCalc.sub.subtraction.sub(a,b))
+print("division operation is:",MyCalc.div.division.div(a,b))
+print("multiplication operation is:",MyCalc.mul.multiplication.mul(a,b))
